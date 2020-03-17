f0bd7e9278a3049b6c239e90fbcd9290cc0169e3
nlgt11 <thuan.ngolegia@gmail.com>
2020-03-17 18:22:00 +0700

    Add profile routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,7 +9,7 @@ const { check, validationResult } = require('express-validator')
 const User = require('../../models/User');
 
 // @route   POST api/users
-// @desc    Test route
+// @desc    Sigup user
 // @access  Public
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
